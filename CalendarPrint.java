030b789c8c25e085a362136b30b3920b1e10723f
andoodoo <anhuang4@galileoweb.org>
2015-08-21 16:34:41 +0000

    September Calendar

--- CalendarPrint.java
+++ CalendarPrint.java
@@ -1,16 +1,18 @@
 public class CalendarPrint {
   public static void main (String[] args) {
-  int x = 1 ;
-  String y = "";
-  while (x <= 30) {
-  if (x == 5 || x == 6 || x == 12 || x == 13 || x == 19 || x == 20 || x == 26 || x == 27)
-  y = " Weekend";
-  else if (x == 7)
-  y = " No School(Labor Day)";
-  else if (x == 25)
-  y = " End of Marking Period";
-    System.out.print("September x ");
-  x++;
+    int x = 1 ;
+    String y = "";
+    while (x <= 30) {
+      if (x == 5 || x == 6 || x == 12 || x == 13 || x == 19 || x == 20 || x == 26 || x == 27)
+        y = "-Weekend";
+      else if (x == 7)
+        y = "-No School(Labor Day)";
+      else if (x == 25)
+        y = "-End of Marking Period";
+      else 
+        y = "-Hell";
+      System.out.println("September" + x + y );
+      x++;
   }
    }
 }
